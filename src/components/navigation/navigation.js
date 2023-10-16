086698d471df6608ab31554ccd215acf3f013bb0
matvei broide <steelwill60@yahoo.com>
2023-10-17 00:56:03 +0300

    11

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -5,9 +5,10 @@ import { useMediaQuery } from 'react-responsive'
 
 
 const Navigation = ({isNavVisible}) => {
+    console.log(isNavVisible)
 
     return (
-        <nav style={{transform:`${isNavVisible ? 'translate(100%)' : 'translate(0)'}`}} className="main__navigation" >
+        <nav style={{transform:`${isNavVisible ? 'translate(0)' : 'translate(100%)'}`}} className="main__navigation" >
             <ul className="main__navigation__items">
                 <li className="main__navigation__items__item"><a className="main__navigation__items__item__link" href="#pros">ПРОЕКТЫ</a></li>
                 <li className="main__navigation__items__item"><a className="main__navigation__items__item__link" href="#form">ОСТАВИТЬ ЗАЯВКУ</a></li>
